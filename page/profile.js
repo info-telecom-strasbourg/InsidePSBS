c71905dc9f6bde9ec31d22c83ad3bffb814977ea
FelixLusseau <felix.lusseau@gmail.com>
2023-01-07 17:50:11 +0100

    mini corrections profil

--- page/profile.js
+++ page/profile.js
@@ -3,10 +3,10 @@ import { View, Text, Image, Button, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
-import { styles,primaryColor } from '../style';
+import { styles, primaryColor } from '../style';
 import { color } from 'react-native-elements/dist/helpers';
 
-const ProfilePage = ({  }) => {
+const ProfilePage = ({ }) => {
   const [Nom, setNom] = React.useState('');
   const [Prenom, setPrenom] = React.useState('');
 
@@ -15,22 +15,22 @@ const ProfilePage = ({  }) => {
   AsyncStorage.getItem('prenom').then((value) => setPrenom(value));
 
   return (
-    
-    <View style={{flexDirection:"column", backgroundColor:primaryColor,flex:1,justifyContent:'space-between',alignContent:'center'}}>
-      <View style={{flex:1}}>
-      <Text style={{marginTop:30,color:'white',alignSelf:'center',lineHeight:30,fontSize:20}}>Nom: {Nom} {"\n"} Prenom: {Prenom}</Text>
-    </View>      
-      <View style={{flex:3}}>
-      <TouchableOpacity style={styles.appButtonContainer} onPress={() => AsyncStorage.setItem('logged','false').then(navigation.navigate('Connexion'))}>
-          <Text style={styles.appButtonText}>Se déconnecter</Text>
-      </TouchableOpacity>
-      </View>
-      <Text style={{fontSize:20,textAlign:'center', flex:1,color:'white',alignSelf:'center',justifyContent:'flex-end',flexDirection:'column'}}>Application développé par {'\n'}Gatien Chenu et Félix Lusseau.{'\n'} Graphisme réalisé par {'\n'}Jeanne König-Wacheux</Text>
-      <Text style={{flex:1,color:'white',alignSelf:'center',justifyContent:'flex-end',flexDirection:'column'}}>Version 1.0.0</Text>
 
-    <View style={{backgroundColor:primaryColor,flex:1}}></View>
+    <View style={{ flexDirection: "column", backgroundColor: primaryColor, flex: 1, justifyContent: 'space-between', alignContent: 'center' }}>
+      <View style={{ flex: 1 }}>
+        <Text style={{ marginTop: 30, color: 'white', alignSelf: 'center', lineHeight: 30, fontSize: 20 }}>Nom: {Nom} {"\n"} Prénom: {Prenom}</Text>
+      </View>
+      <View style={{ flex: 3 }}>
+        <TouchableOpacity style={styles.appButtonContainer} onPress={() => AsyncStorage.setItem('logged', 'false').then(navigation.navigate('Connexion'))}>
+          <Text style={styles.appButtonText}>Se déconnecter</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={{ fontSize: 20, textAlign: 'center', flex: 1, color: 'white', alignSelf: 'center', justifyContent: 'flex-end', flexDirection: 'column' }}>Application développée par {'\n'}Gatien Chenu et Félix Lusseau.{'\n'} Graphismes réalisés par {'\n'}Jeanne König-Wacheux</Text>
+      <Text style={{ flex: 1, color: 'white', alignSelf: 'center', justifyContent: 'flex-end', flexDirection: 'column' }}>Version 1.0.0</Text>
+
+      <View style={{ backgroundColor: primaryColor, flex: 1 }}></View>
     </View>
-    
+
   );
 };
 
