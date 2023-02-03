95715926771de035bc9b218108da390112ddc476
gatienc <gatien.chenu@gmail.com>
2023-02-03 23:57:08 +0100

    patch forgotten file save in fouaille

--- pages/fouaille.js
+++ pages/fouaille.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from 'react';
-import { FlatList, ScrollView,StyleSheet,Text, View, TextInput,RefreshControl,TouchableOpacity, StatusBar, Button, Image } from 'react-native';
+import { StyleSheet,Text, View, TextInput,RefreshControl,TouchableOpacity, StatusBar, Button, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import {LoadingPage} from './loadingpage';
@@ -131,17 +131,7 @@ function Transac( transac ) {
   );
 }
 
-function Transac( transac ) {
-  const time = fromNow(transac.date_histo);
-  var color
-  transac.delta.includes('-') ? color='red' : color='green';
-  return (
-    <View style={styles.TransacContainer}>
-      <Text style={{color:color,marginLeft:10,alignSelf:'center',fontSize:13}}>{transac.delta}</Text>
-      <Text style={{marginRight:10,alignSelf:'center',fontSize:13}}>{time}</Text>
-    </View>
-  );
-}
+
 
 
 export default Fouaille;
