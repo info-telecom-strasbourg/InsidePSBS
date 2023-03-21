a28efb15c736bc7f3bc930a823494291ba5f9b48
gatienc <gatien.c@shnoulle.net>
2023-03-22 00:45:44 +0100

    add UseEffect in data update

--- pages/agenda.js
+++ pages/agenda.js
@@ -14,13 +14,15 @@ function AgendaTPS (){
   const [isLoading, setIsLoading] = React.useState(true);
   const [events, setEvents] = React.useState([]);
   const [eventsNumber, setEventsNumber] = React.useState(0);
-  try {
-    console.log("on essaie de chercher sur internet au début");
-    UpdateAgenda();
-  }
-  catch (error) {
-    console.log("il n'y a pas de connexion/problème serveur");
-  }
+  useEffect( () => {
+    try {
+      console.log("on essaie de mettre à jour les données au début");
+      UpdateAgenda();
+    }
+    catch (error) {
+      console.log("il n'y a pas de connexion/problème serveur, mise à jour impossible");
+    }
+  }, []);
   async function UpdateValue(){
     console.log("UpdatingValue");
     [_events,_eventsNumber]= await UpdateAgenda();
@@ -44,7 +46,7 @@ function AgendaTPS (){
         setIsLoading(false);
       }
       else {
-        console.log("on va cherche en local");
+        console.log("on va cherche les données en local (normalement elle viennent juste d'être sauvegardé");
         _jsonevents = await AsyncStorage.getItem("events");
         setEventsNumber(LocalEventsNumber);
         setEvents(JSON.parse(_jsonevents));
