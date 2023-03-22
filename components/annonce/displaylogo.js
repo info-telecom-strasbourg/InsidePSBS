c666b7d871a010fdf88795d117ac57a940fd1671
gatienc <gatien.c@shnoulle.net>
2023-03-22 19:20:37 +0100

    env bugfixes ( bad variale name)

--- components/annonce/displaylogo.js
+++ components/annonce/displaylogo.js
@@ -1,6 +1,7 @@
 import { View, Image, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 import {Avatar, } from 'react-native-paper';
+import {BackendURL} from 'react-native-dotenv';
 
 /**
  * fonction qui renvoie l'image du logo de l'association ou du club
@@ -10,10 +11,11 @@ import {Avatar, } from 'react-native-paper';
 export function DisplayLogo(props) {
     
     // Image=<Image style={styles.Imagecentrale} source={{ uri: 'https://i.ibb.co/QnbX89q/triste.png' }} resizeMode='cover'         />
-    path=BackendUrl+'logo/'+props.asso_club+'.png';
+    var path=BackendURL+'logo/'+props.asso_club+'.png';
+    console.log(path);
 
     return (
-        <Avatar.Image  style={{backgroundColor:'transparent'}} size={50} source={{uri : BackendUrl+'logo/'+props.asso_club+'.png' }} />
+        <Avatar.Image  style={{backgroundColor:'transparent'}} size={50} source={{uri : BackendURL+'logo/'+props.asso_club+'.png' }} />
     );
 }  
 
