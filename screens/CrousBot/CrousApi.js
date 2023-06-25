7e3654fee6c41f8d1a211ddc1228196a1b96f721
Romain Bourdain <romainbourdain@MacBook-Air-de-Romain.local>
2023-06-25 16:12:36 +0200

    day selector

--- screens/CrousBot/CrousApi.js
+++ screens/CrousBot/CrousApi.js
@@ -1,96 +1,111 @@
-import axios from 'axios';
+import axios from "axios";
 
 // HTML Parser done by LosKeeper
 
-
-
-export function getDate(day_diff){
-    // Get the date of the day
-    var date = new Date();
-    date.setDate(date.getDate()+day_diff);
-    var day = date.getDate().toString();
-    if (day[0] === "0") {
-        day = day[1];
-    }
-    var month = date.toLocaleString("default", { month: "long" });
-    var date = parseInt(day) + " " + month;   
-    return date;
+export function getDate(day_diff) {
+  // Get the date of the day
+  var date = new Date();
+  date.setDate(date.getDate() + day_diff);
+  var day = date.getDate().toString();
+  if (day[0] === "0") {
+    day = day[1];
+  }
+  var month = date.toLocaleString("default", { month: "long" });
+  var date = parseInt(day) + " " + month;
+  return date;
 }
+
 export async function getMenuIllkirch(date) {
-    var URL = "https://www.crous-strasbourg.fr/restaurant/resto-u-illkirch/";
-    var response = await axios.get(URL);
-    var buffer = response.data;
-    console.log(date);
+  var URL = "https://www.crous-strasbourg.fr/restaurant/resto-u-illkirch/";
+  var response = await axios.get(URL);
+  var buffer = response.data;
 
-    let menu = buffer.indexOf(date);
-    buffer = buffer.substring(menu);
+  const formatedDate = `${date.getDate()} ${date.toLocaleString("default", {
+    month: "long",
+  })}`;
 
-    // Get the menu of the lunch
-    menu = buffer.indexOf(date);
-    if (menu === -1) {
-        return -1;
-    }
-    menu+=45
-    buffer = buffer.substring(menu);
+  let menu = buffer.indexOf(formatedDate);
 
-    // Get the end of the menu
-    let end = buffer.indexOf("SALLE DES PERSONNELS");
+  buffer = buffer.substring(menu);
 
-    buffer = buffer.substring(0, end);
+  // Get the menu of the lunch
+  menu = buffer.indexOf(formatedDate);
+  if (menu === -1) {
+    return -1;
+  }
+  menu += 45;
+  buffer = buffer.substring(menu);
 
-    // Remove the HTML tags
-    buffer = buffer.replace(/<\/li>/g, "\n");
-    buffer = buffer.replace(/<li>/g, "");
-    buffer = buffer.replace(/<\/ul>/g, "\n");
-    buffer = buffer.replace(/<ul>/g, "\n");
+  // Get the end of the menu
+  let end = buffer.indexOf("SALLE DES PERSONNELS");
 
-    // Get only the menu concerning the students only
-    buffer = buffer.split("SALLE");
-    let str = "";
-    for (let i = 0; i < buffer.length; i++) {
+  buffer = buffer.substring(0, end);
+
+  // Remove the HTML tags
+  buffer = buffer.replace(/<\/li>/g, "\n");
+  buffer = buffer.replace(/<li>/g, "");
+  buffer = buffer.replace(/<\/ul>/g, "\n");
+  buffer = buffer.replace(/<ul>/g, "\n");
+
+  // Get only the menu concerning the students only
+  buffer = buffer.split("SALLE");
+  let str = "";
+  for (let i = 0; i < buffer.length; i++) {
     if (buffer[i].includes("ETUDIANTS")) {
-        str += "SALLE" + buffer[i];
+      str += "SALLE" + buffer[i];
     }
-    }
+  }
 
-    // Remove the ":" in a room content
-    if (str.includes("chaude : ")) {
+  // Remove the ":" in a room content
+  if (str.includes("chaude : ")) {
     str = str.replace("chaude : ", "chaude -> ");
-    }
+  }
 
-    // Check if the menu is empty
-    if (str === "") {
+  // Check if the menu is empty
+  if (str === "") {
     str = "Pas de menu disponible pour aujourd'hui !";
-    }
-    return str;
+  }
+  return str;
 }
 
 export function menuFormatter(menu) {
-    let dish={}
-    let starindex = menu.indexOf("SALLE DES ETUDIANTS - ENTREES")+"SALLE DES ETUDIANTS - ENTREES".length;
-    let endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU DU JOUR");
-    let starter = menu.substring(starindex, endindex);
-    
-    dish.starter = starter;
-    starindex = menu.indexOf("SALLE DES ETUDIANTS - MENU DU JOUR")+"SALLE DES ETUDIANTS - MENU DU JOUR".length;
-    endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - POLE PATES");
-    let main = menu.substring(starindex, endindex);
-    dish.main = "   "+main;
-    starindex = menu.indexOf("SALLE DES ETUDIANTS - POLE PATES")+"SALLE DES ETUDIANTS - POLE PATES".length;
-    endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU VEGETARIEN");
-    let pasta = menu.substring(starindex, endindex);
-    dish.pasta = "   "+pasta;
-    starindex = menu.indexOf("SALLE DES ETUDIANTS - MENU VEGETARIEN")+"SALLE DES ETUDIANTS - MENU VEGETARIEN".length;
-    endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - DESSERTS");
-    let veg = menu.substring(starindex, endindex);
-    dish.veg = "   "+veg;
-    starindex = menu.indexOf("SALLE DES ETUDIANTS - DESSERTS")+"SALLE DES ETUDIANTS - DESSERTS".length;
-    endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - GRILL");
-    let dessert = menu.substring(starindex, endindex);
-    dish.dessert = "   "+dessert;
-    starindex = menu.indexOf("SALLE DES ETUDIANTS - GRILL")+ "SALLE DES ETUDIANTS - GRILL".length;
-    endindex = menu.length-2;//to delete the 2 last \n
-    let grill = menu.substring(starindex, endindex);
-    dish.grill = "   "+grill;
-    return dish;
+  let dish = {};
+  let starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - ENTREES") +
+    "SALLE DES ETUDIANTS - ENTREES".length;
+  let endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU DU JOUR");
+  let starter = menu.substring(starindex, endindex);
+
+  dish.starter = starter;
+  starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - MENU DU JOUR") +
+    "SALLE DES ETUDIANTS - MENU DU JOUR".length;
+  endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - POLE PATES");
+  let main = menu.substring(starindex, endindex);
+  dish.main = "   " + main;
+  starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - POLE PATES") +
+    "SALLE DES ETUDIANTS - POLE PATES".length;
+  endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU VEGETARIEN");
+  let pasta = menu.substring(starindex, endindex);
+  dish.pasta = "   " + pasta;
+  starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - MENU VEGETARIEN") +
+    "SALLE DES ETUDIANTS - MENU VEGETARIEN".length;
+  endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - DESSERTS");
+  let veg = menu.substring(starindex, endindex);
+  dish.veg = "   " + veg;
+  starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - DESSERTS") +
+    "SALLE DES ETUDIANTS - DESSERTS".length;
+  endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - GRILL");
+  let dessert = menu.substring(starindex, endindex);
+  dish.dessert = "   " + dessert;
+  starindex =
+    menu.indexOf("SALLE DES ETUDIANTS - GRILL") +
+    "SALLE DES ETUDIANTS - GRILL".length;
+  endindex = menu.length - 2; //to delete the 2 last \n
+  let grill = menu.substring(starindex, endindex);
+  dish.grill = "   " + grill;
+  return dish;
 }
--- screens/CrousBot/CrousBotScreen.jsx
+++ screens/CrousBot/CrousBotScreen.jsx
@@ -1,101 +1,104 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TEXT } from "../../constants";
-import { useState } from "react";
-import { Button, Text, View,TouchableOpacity } from "react-native";
 import { useTheme } from "../../contexts";
-import { text_styles } from "../../styles";
 import {
   BackButtonTopbar,
   Loader,
   ScrollScreenContainer,
 } from "../../components";
-import { getDate, getMenuIllkirch, menuFormatter } from "./CrousApi";
-import { ChevronLeftIcon,ChevronRightIcon } from "../../assets/icons";
+import { ChevronLeftIcon, ChevronRightIcon } from "../../assets/icons";
+import { Text, TouchableOpacity, View } from "react-native";
+import { text_styles } from "../../styles";
 import styles from "./touchableicon.style";
-
-
+import { getMenuIllkirch, menuFormatter } from "./CrousApi";
 
 const CrousBotScreen = () => {
   const { theme } = useTheme();
-  const [isLoading, setisLoading] = useState(true);
-  const [isMenu, setisMenu] = useState(false);
-  const [data, setData] = useState('null');
-  const [day_diff, setDay_diff] = useState(0);
-  const [date, setDate] = useState(getDate(day_diff));
-  
+  const [data, setData] = useState(null);
+  const [date, setDate] = useState(new Date());
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    setDate(getDate(day_diff))
-    console.log(date)
     getMenuIllkirch(date).then((res) => {
-    if (res!=-1){
-      res=menuFormatter(res);
-      setisMenu(true);
-      setData(res);}
-    else{setisMenu(false)};
-    setisLoading(false);
-  }, []);}, [day_diff]);
+      if (res === -1) setData(null);
+      else {
+        res = menuFormatter(res);
+        setData(res);
+      }
+    });
+  }, [date]);
+
+  const nextDay = (date) => {
+    return new Date(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+  };
+
+  const prevDay = (date) => {
+    return new Date(date.getTime() - 1 * 24 * 60 * 60 * 1000);
+  };
+
+  const formatDate = (date) => {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    day = String(day).length === 1 ? "0" + day : day;
+    month = String(month).length === 1 ? "0" + month : month;
+    return `${day}/${month}`;
+  };
 
   return (
     <ScrollScreenContainer>
       <BackButtonTopbar>{TEXT.crousbot.page_name}</BackButtonTopbar>
-      <View style={{alignSelf:'center', flexDirection: 'row'}}>
-          <TouchableOpacity
+      <View style={{ alignSelf: "center", flexDirection: "row" }}>
+        <TouchableOpacity
           style={styles.iconContainer()}
-          onPress={() => setDay_diff(day_diff-1)}    >
+          onPress={() => setDate(prevDay(date))}
+        >
           <ChevronLeftIcon width={13} height={24} color={theme.text} />
-          </TouchableOpacity>
-      <Text style={text_styles.title3(theme)}>{date}</Text>
-          <TouchableOpacity
+        </TouchableOpacity>
+        <Text style={text_styles.title3(theme)}>{formatDate(date)}</Text>
+        <TouchableOpacity
           style={styles.iconContainer()}
-          onPress={() => setDay_diff(day_diff+1)}    >
+          onPress={() => setDate(nextDay(date))}
+        >
           <ChevronRightIcon width={13} height={24} color={theme.text} />
-          </TouchableOpacity>
+        </TouchableOpacity>
       </View>
       <View style={{ paddingHorizontal: 11 }}>
-        <Text style={text_styles.title3(theme)}>{day_diff}</Text>
-          <View style={{ paddingVertical: 30 }}>
+        <Text style={text_styles.title3(theme)}>{formatDate(date)}</Text>
+        <View style={{ paddingVertical: 30 }}>
           {isLoading ? (
-            <Loader />): isMenu ? ( <>
+            <Loader />
+          ) : data ? (
+            <>
               <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.starter}
+                {TEXT.crousbot.starter}
               </Text>
-              <Text style={text_styles.body3(theme)}>
-              {data.starter}
-              </Text>
+              <Text style={text_styles.body3(theme)}>{data.starter}</Text>
               <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.main}
+                {TEXT.crousbot.main}
               </Text>
-              <Text style={text_styles.body3(theme)}>
-             {data.main}
-              </Text>
+              <Text style={text_styles.body3(theme)}>{data.main}</Text>
               <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.pasta}
+                {TEXT.crousbot.pasta}
               </Text>
-              <Text style={text_styles.body3(theme)}>
-             {data.pasta}
-              </Text>
+              <Text style={text_styles.body3(theme)}>{data.pasta}</Text>
+              <Text style={text_styles.title3(theme)}>{TEXT.crousbot.veg}</Text>
+              <Text style={text_styles.body3(theme)}>{data.veg}</Text>
               <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.veg}
+                {TEXT.crousbot.grill}
               </Text>
-              <Text style={text_styles.body3(theme)}>
-              {data.veg}
-              </Text>
+              <Text style={text_styles.body3(theme)}>{data.grill}</Text>
               <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.grill}
-             </Text>
-              <Text style={text_styles.body3(theme)}>
-              {data.grill}
+                {TEXT.crousbot.dessert}
               </Text>
-              <Text style={text_styles.title3(theme)}>
-              {TEXT.crousbot.dessert}
-              </Text>
-              <Text style={text_styles.body3(theme)}>
-             {data.dessert}</Text>
-             </>):(<Text style={text_styles.title3(theme)}>{TEXT.crousbot.menu_error}</Text>
-             )}
-          </View>
+              <Text style={text_styles.body3(theme)}>{data.dessert}</Text>
+            </>
+          ) : (
+            <Text style={text_styles.title3(theme)}>
+              {TEXT.crousbot.menu_error}
+            </Text>
+          )}
         </View>
-
+      </View>
     </ScrollScreenContainer>
   );
 };
