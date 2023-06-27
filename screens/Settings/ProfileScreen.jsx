8bec11ff111d914d8a48a085e1c81407fae68b47
Romain Bourdain <romainbourdain@MacBook-Air-de-Romain.local>
2023-06-27 16:17:32 +0200

    profile

--- screens/Settings/ProfileScreen.jsx
+++ screens/Settings/ProfileScreen.jsx
@@ -1,13 +1,100 @@
 import React from "react";
-import { BackButtonTopbar, ScrollScreenContainer } from "../../components";
-import { TEXT } from "../../constants";
+import {
+  BackButtonTopbar,
+  ColoredButton,
+  Loader,
+  PrimaryButton,
+  ScrollScreenContainer,
+} from "../../components";
+import { COLORS, TEXT } from "../../constants";
+import { useLocalStorage } from "../../contexts/localStorageContext";
+import { useTheme } from "../../contexts";
+import { useRouter } from "expo-router";
+import { useFetch } from "../../hooks";
+import { Image, Text, View } from "react-native";
+import styles from "./settings.style";
+import { text_styles } from "../../styles";
+import SettingButton from "./SettingButton";
 
 const ProfileScreen = () => {
+  const { data } = useLocalStorage();
+  const { theme } = useTheme();
+  const router = useRouter();
+  const { res, isLoading, error } = useFetch(
+    "https://app-pprd.its-tps.fr/api/user/me",
+    {
+      Accept: "application/json",
+      Authorization: `Bearer ${data.token}`,
+    }
+  );
+
   return (
     <ScrollScreenContainer>
       <BackButtonTopbar rightIcon={<></>}>
         {TEXT.settings.profile.title}
       </BackButtonTopbar>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.wrapper}>
+            <Image
+              source={{ uri: res?.data.avatar_url }}
+              style={{ height: 80, width: 80 }}
+            />
+            <Text style={text_styles.title3(theme)}>
+              {res?.data.first_name} {res?.data.last_name}
+            </Text>
+            <Text style={text_styles.body2({ text: theme.text_secondary })}>
+              @{res?.data.user_name}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <SettingButton
+              text="Nom d'utilisateur"
+              rightIcon={
+                <Text style={text_styles.body3({ text: theme.text_secondary })}>
+                  @{res?.data.user_name}
+                </Text>
+              }
+            />
+            <SettingButton
+              text="Numéro de téléphone"
+              rightIcon={
+                <Text style={text_styles.body3({ text: theme.text_secondary })}>
+                  {res?.data.phone}
+                </Text>
+              }
+            />
+            <SettingButton
+              text="Filière"
+              rightIcon={
+                <Text style={text_styles.body3({ text: theme.text_secondary })}>
+                  {res?.data.sector}
+                </Text>
+              }
+            />
+            <SettingButton
+              text="Promotion"
+              rightIcon={
+                <Text style={text_styles.body3({ text: theme.text_secondary })}>
+                  {res?.data.promotion_year}
+                </Text>
+              }
+            />
+            <SettingButton text="Mot de passe" />
+            <SettingButton text="Compte Unistra" />
+          </View>
+          <PrimaryButton text="Déconnexion" textStyle={{ fontSize: 17 }} />
+          <View style={{ height: 10 }} />
+          <ColoredButton
+            foreground={COLORS.dark_red}
+            background={COLORS.light_red}
+            text="Supprimer le compte"
+            textStyle={{ fontSize: 17 }}
+          />
+        </View>
+      )}
     </ScrollScreenContainer>
   );
 };
