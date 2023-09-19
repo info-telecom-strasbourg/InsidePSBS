05246df868093b7d43d362c29ccf99a44d71c9fb
gatienc <gatien.chenu@gmail.com>
2023-09-19 11:26:39 +0200

    bugfix: birthdate error when clicked but not defined

--- utils/date/createDateFromDDMMYYYY.js
+++ utils/date/createDateFromDDMMYYYY.js
@@ -2,7 +2,7 @@ function createDateFromDDMMYYYY(dateString) {
   // res date format : YYYY//MM/DD
   const parts = dateString.split("/");
   if (parts.length !== 3) {
-    return "0-0-0";
+    return "";
   }
   res = parts[2] + "-" + parts[1] + "-" + parts[0];
   return res;
