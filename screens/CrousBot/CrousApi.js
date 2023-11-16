70eaf533f2da7e1ec66faeaa30c0b9f6000b0d66
unknown <thomas.dumond2001@gmail.com>
2023-11-16 11:48:04 +0100

    remove old code

--- screens/CrousBot/CrousApi.js
+++ screens/CrousBot/CrousApi.js
@@ -21,54 +21,6 @@ export async function getMenu(name) {
   response = await axios.get(URL);
 
   return response.data;
-
-
-  // const formatedDate = `${date.getDate()} ${date.toLocaleString("fr-FR", {
-  //   month: "long",
-  // })}`;
-
-  // let menu = buffer.indexOf(formatedDate);
-
-  // buffer = buffer.substring(menu);
-
-  // // Get the menu of the lunch
-  // menu = buffer.indexOf(formatedDate);
-  // if (menu === -1) {
-  //   return -1;
-  // }
-  // menu += 45;
-  // buffer = buffer.substring(menu);
-
-  // // Get the end of the menu
-  // let end = buffer.indexOf("SALLE DES PERSONNELS");
-
-  // buffer = buffer.substring(0, end);
-
-  // // Remove the HTML tags
-  // buffer = buffer.replace(/<\/li>/g, "\n");
-  // buffer = buffer.replace(/<li>/g, "");
-  // buffer = buffer.replace(/<\/ul>/g, "\n");
-  // buffer = buffer.replace(/<ul>/g, "\n");
-
-  // // Get only the menu concerning the students only
-  // buffer = buffer.split("SALLE");
-  // let str = "";
-  // for (let i = 0; i < buffer.length; i++) {
-  //   if (buffer[i].includes("ETUDIANTS")) {
-  //     str += "SALLE" + buffer[i];
-  //   }
-  // }
-
-  // // Remove the ":" in a room content
-  // if (str.includes("chaude : ")) {
-  //   str = str.replace("chaude : ", "chaude -> ");
-  // }
-
-  // // Check if the menu is empty
-  // if (str === "") {
-  //   str = "Pas de menu disponible pour aujourd'hui !";
-  // }
-  // return str;
 }
 
 export function menuFormatter(menu) {
@@ -102,43 +54,4 @@ export function menuFormatter(menu) {
   }
 
   return dish;
-
-  // let starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - ENTREES") +
-  //   "SALLE DES ETUDIANTS - ENTREES".length;
-  // let endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU DU JOUR");
-  // let starter = menu.substring(starindex, endindex);
-
-  // dish.starter = starter;
-  // starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - MENU DU JOUR") +
-  //   "SALLE DES ETUDIANTS - MENU DU JOUR".length;
-  // endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - POLE PATES");
-  // let main = menu.substring(starindex, endindex);
-  // dish.main = "   " + main;
-  // starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - POLE PATES") +
-  //   "SALLE DES ETUDIANTS - POLE PATES".length;
-  // endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - MENU VEGETARIEN");
-  // let pasta = menu.substring(starindex, endindex);
-  // dish.pasta = "   " + pasta;
-  // starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - MENU VEGETARIEN") +
-  //   "SALLE DES ETUDIANTS - MENU VEGETARIEN".length;
-  // endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - DESSERTS");
-  // let veg = menu.substring(starindex, endindex);
-  // dish.veg = "   " + veg;
-  // starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - DESSERTS") +
-  //   "SALLE DES ETUDIANTS - DESSERTS".length;
-  // endindex = menu.indexOf("\n\nSALLE DES ETUDIANTS - GRILL");
-  // let dessert = menu.substring(starindex, endindex);
-  // dish.dessert = "   " + dessert;
-  // starindex =
-  //   menu.indexOf("SALLE DES ETUDIANTS - GRILL") +
-  //   "SALLE DES ETUDIANTS - GRILL".length;
-  // endindex = menu.length - 2; //to delete the 2 last \n
-  // let grill = menu.substring(starindex, endindex);
-  // dish.grill = "   " + grill;
-  // return dish;
 }
