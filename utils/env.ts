0ac237a94accf95e24ce5a16e3b8ce329632f6fd
Romain <romain.bourdain93@gmail.com>
2024-01-04 00:01:37 +0100

    update environment

--- utils/env.ts
+++ utils/env.ts
@@ -1,19 +1,22 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    STAGE: z.enum(["dev", "prod"]),
-    API_URL: z.string().min(1),
-    CROUS_API_URL: z.string().min(1),
-  },
-  client: {},
-  runtimeEnv: {
-    STAGE: process.env.STAGE,
-    API_URL:
-      process.env.STAGE === "prod"
-        ? "https://app.its-tps.fr"
-        : "https://app-pprd.its-tps.fr",
-    CROUS_API_URL: process.env.CROUS_API_URL,
-  },
+const envSchema = z.object({
+  STAGE: z.enum(["dev", "prod"]),
+  API_URL: z.string().min(1),
+  CROUS_API_URL: z.string().min(1),
 });
+
+const getEnv = () => {
+  try {
+    console.log(process.env.EXPO_PUBLIC_API_URL);
+    return envSchema.parse({
+      STAGE: process.env.EXPO_PUBLIC_STAGE,
+      API_URL: process.env.EXPO_PUBLIC_API_URL,
+      CROUS_API_URL: process.env.EXPO_PUBLIC_CROUS_API_URL,
+    });
+  } catch (e) {
+    console.error("Error while parsing env variables:\n", e.message);
+  }
+};
+
+export const env = getEnv();
