6c2b8a9bec71ac5045a64df1996a1609c6643928
Fabio <>
2024-06-19 09:48:59 +0200

    cleanup of homescreen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,38 +1,10 @@
-import Card from "@/components/primitives/card";
+import GridCards from "@/components/home/grid-cards";
 import { PageContainer } from "@/components/primitives/container";
 import { Typography } from "@/components/primitives/typography";
 import { Header } from "@/features/layout/header";
-import { useFetch } from "@/hooks/useFetch";
-import { FouailleSchema, type FouailleData } from "@/schemas/fouaille.schema";
-import { colors } from "@/theme/colors";
-import { useRouter } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { CameraIcon, CreditCard, Users, Utensils } from "lucide-react-native";
 import { ScrollView, View } from "react-native";
 
 const HomeScreen = () => {
-  const router = useRouter();
-
-  const url = `${process.env.EXPO_PUBLIC_API_URL}/api/fouaille`;
-
-  const fetcher = async (url: string): Promise<FouailleData> => {
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer 87|m7VZi5MWAmtz3gFkKolgqjAWCeJucV90ZOEdzcCx",
-      },
-    });
-    const data = await res.json();
-    const parsedData = FouailleSchema.safeParse(data.data);
-    if (!parsedData.success) {
-      throw new Error(parsedData.error.message);
-    }
-    return data.data;
-  };
-
-  const { data, isLoading } = useFetch(url, fetcher);
-
   return (
     <PageContainer className="bg-background">
       <ScrollView>
@@ -41,49 +13,8 @@ const HomeScreen = () => {
           leftIcon="inside-psbs"
           rightIcon="settings"
         />
-        <StatusBar style="auto" />
-        <View className="mb-8 flex-col items-center gap-4">
-          <View className="flex-1 flex-row gap-4">
-            <Card
-              subtitle="Fouaille"
-              color="purple"
-              backgroundColor={colors.lightPurple}
-              icon={CreditCard}
-              onPress={() => router.push("/fouaille")}
-            >
-              {isLoading ? "Loading..." : `${data?.balance}€`}
-            </Card>
-            <Card
-              icon={Users}
-              color="green"
-              backgroundColor={colors.lightGreen}
-              onPress={() => router.push("/assos")}
-            >
-              Clubs et Assos
-            </Card>
-          </View>
-          <View className="flex-1 flex-row gap-4">
-            <Card
-              color="orange"
-              icon={CameraIcon}
-              backgroundColor={colors.lightOrange}
-              onPress={() =>
-                router.replace("https://nextcloud.its-tps.fr/s/zfFkwR6y5wxt5gW")
-              }
-            >
-              Photo
-            </Card>
-            <Card
-              color="red"
-              backgroundColor={colors.lightRed}
-              icon={Utensils}
-              onPress={() => router.push("/menu")}
-            >
-              Menu du RU
-            </Card>
-          </View>
-        </View>
-        <View className="">
+        <GridCards />
+        <View>
           <Typography size="h1" fontWeight="bold">
             Actualités
           </Typography>
--- src/components/home/grid-cards.tsx
+++ src/components/home/grid-cards.tsx
@@ -1,9 +1,76 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@/auth/useAuth";
+import { useFetch } from "@/hooks/useFetch";
+import { type FouailleData, FouailleSchema } from "@/schemas/fouaille.schema";
+import { colors } from "@/theme/colors";
+import { useRouter } from "expo-router";
+import { CameraIcon, CreditCard, Users, Utensils } from "lucide-react-native";
+import { View } from "react-native";
+import Card from "../primitives/card";
 
 const GridCards = () => {
+  const router = useRouter();
+  const url = `${process.env.EXPO_PUBLIC_API_URL}/api/fouaille`;
+
+  const { token } = useAuth();
+
+  const fetcher = async (url: string): Promise<FouailleData> => {
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+    const parsedData = FouailleSchema.safeParse(data.data);
+    if (!parsedData.success) {
+      throw new Error(parsedData.error.message);
+    }
+    return data.data;
+  };
+  const { data, isLoading } = useFetch(url, fetcher);
+
   return (
-    <View>
-      <Text>GridCards</Text>
+    <View className="mb-8 flex-col items-center gap-4">
+      <View className="flex-1 flex-row gap-4">
+        <Card
+          subtitle="Fouaille"
+          color="purple"
+          backgroundColor={colors.lightPurple}
+          icon={CreditCard}
+          onPress={() => router.push("/fouaille")}
+        >
+          {isLoading ? "Loading..." : `${data?.balance}€`}
+        </Card>
+        <Card
+          icon={Users}
+          color="green"
+          backgroundColor={colors.lightGreen}
+          onPress={() => router.push("/assos")}
+        >
+          Clubs et Assos
+        </Card>
+      </View>
+      <View className="flex-1 flex-row gap-4">
+        <Card
+          color="orange"
+          icon={CameraIcon}
+          backgroundColor={colors.lightOrange}
+          onPress={() =>
+            router.replace("https://nextcloud.its-tps.fr/s/zfFkwR6y5wxt5gW")
+          }
+        >
+          Photo
+        </Card>
+        <Card
+          color="red"
+          backgroundColor={colors.lightRed}
+          icon={Utensils}
+          onPress={() => router.push("/menu")}
+        >
+          Menu du RU
+        </Card>
+      </View>
     </View>
   );
 };
