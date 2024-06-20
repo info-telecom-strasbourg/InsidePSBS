428144f478e39a0d1bb1b22356a8a7db0ee96206
Fabio <>
2024-06-20 17:02:00 +0200

    fixing iOS version

--- src/components/primitives/container.tsx
+++ src/components/primitives/container.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/utils/cn";
 import type { PropsWithChildren } from "react";
-import { SafeAreaView, View, type ViewProps } from "react-native";
+import { Platform, SafeAreaView, View, type ViewProps } from "react-native";
 
 export type PageContainerProps = PropsWithChildren<{} & ViewProps>;
 
@@ -11,7 +11,10 @@ export const PageContainer = ({
 }: PageContainerProps) => {
   return (
     <SafeAreaView className="flex-1 bg-background">
-      <View className={cn("flex-1 px-3 pt-16", className)} {...props}>
+      <View
+        className={cn("flex-1 px-3", Platform.OS && "pt-16", className)}
+        {...props}
+      >
         {children}
       </View>
     </SafeAreaView>
