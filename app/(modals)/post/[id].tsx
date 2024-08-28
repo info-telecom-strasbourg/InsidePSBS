81cb729f0a7a4706d5011f500ec831a1c446e91e
fab-toc <81991643+fab-toc@users.noreply.github.com>
2024-08-28 14:46:55 +0200

    resolved conflicts

--- app/(modals)/post/[id].tsx
+++ app/(modals)/post/[id].tsx
@@ -3,24 +3,12 @@ import { Header } from "@/components/primitives/header";
 import { useComments } from "@app/(modals)/post/_features/comments.query";
 import { type CommentsData } from "@app/(modals)/post/_features/comments.schema";
 import { useOnePost } from "@app/(modals)/post/_features/one-post.query";
-import { Post } from "@app/(tabs)/posts/_features/post";
+import { Post, SkeletonPost } from "@app/(tabs)/posts/_features/post";
 import { FlashList } from "@shopify/flash-list";
 import { useLocalSearchParams } from "expo-router";
-<<<<<<< HEAD
-import { CircleMinus, CirclePlus } from "lucide-react-native";
-import { Skeleton } from "moti/skeleton";
-import { memo, useMemo, useState } from "react";
-import {
-  ActivityIndicator,
-  RefreshControl,
-  TouchableOpacity,
-  View,
-} from "react-native";
-=======
 import { useMemo } from "react";
 import { RefreshControl, View } from "react-native";
 import { Comment, SkeletonComment } from "./_features/comment";
->>>>>>> f8a6610 (recursive component and fetching children done)
 
 export default function PostIdPage() {
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -34,7 +22,7 @@ export default function PostIdPage() {
     size,
     setSize,
     hasMore,
-  } = useComments(id, null);
+  } = useComments(id);
 
   const comments = useMemo(
     () => (commentsData ? commentsData.flat() : []),
@@ -76,133 +64,21 @@ export default function PostIdPage() {
           </View>
         }
         ListEmptyComponent={
-          <View className="mb-4 gap-4">
-            <SkeletonComment />
-            <SkeletonComment />
-            <SkeletonComment />
-            <SkeletonComment />
-            <SkeletonComment />
-            <SkeletonComment />
-          </View>
+          <>
+            <View>
+              <SkeletonPost />
+            </View>
+            <View className="mb-4 gap-4">
+              <SkeletonComment />
+              <SkeletonComment />
+              <SkeletonComment />
+              <SkeletonComment />
+              <SkeletonComment />
+              <SkeletonComment />
+            </View>
+          </>
         }
       />
     </PageContainer>
   );
 }
-<<<<<<< HEAD
-
-export const Comment = memo(function Comment({
-  comment,
-  postId,
-  levelFromRoot = 0,
-}: {
-  comment: CommentsData["data"][0] | undefined;
-  postId: string;
-  levelFromRoot?: number;
-}) {
-  const { theme } = useTheme();
-
-  const { data: children, isLoading: childrenAreLoading } = useComments(
-    postId,
-    comment!.id
-  );
-
-  const childrenComment = useMemo(
-    () => (children ? children.flat() : []),
-    [children]
-  );
-  const [showChildren, setShowChildren] = useState<boolean>(false);
-
-  if (!comment) return null;
-  return (
-    <View className="mb-4 flex-row gap-2">
-      <View className="mt-1">
-        <ProfilePicture
-          avatar={comment?.author.logo_url}
-          imageSize={35}
-          color={colors[theme].popover}
-          isOrganization={comment.author.is_organization}
-          name={comment.author.name}
-        />
-      </View>
-      <View className="flex-1">
-        <View className="gap-3 rounded-2xl bg-popover p-3">
-          <View className="flex-row justify-between">
-            <Typography fontWeight="medium" size="h4">
-              {comment?.author.name}
-            </Typography>
-            <Typography className="text-muted-foreground" size="p">
-              {comment.created_since}
-            </Typography>
-          </View>
-          <Typography size="p">{comment?.body}</Typography>
-        </View>
-        {comment?.children_count && comment.children_count > 0 ? (
-          (!childrenComment || childrenAreLoading) && showChildren ? (
-            <View className="flex-row items-center justify-center">
-              <ActivityIndicator
-                size={30}
-                color={colors[theme].mutedForeground}
-              />
-              <Typography
-                size="h5"
-                fontWeight="medium"
-                className="text-muted-foreground"
-              >
-                Chargement...
-              </Typography>
-            </View>
-          ) : (
-            <TouchableOpacity
-              className="flex-row justify-start gap-2 py-2"
-              onPress={() => setShowChildren(!showChildren)}
-            >
-              {showChildren ? (
-                <CircleMinus color={colors[theme].mutedForeground} />
-              ) : (
-                <CirclePlus color={colors[theme].mutedForeground} />
-              )}
-              <Typography
-                size="h5"
-                fontWeight="medium"
-                className="text-muted-foreground"
-              >
-                {showChildren ? "Masquer" : "Afficher"} les réponses
-              </Typography>
-            </TouchableOpacity>
-          )
-        ) : null}
-        <View className={levelFromRoot >= 2 ? "" : "ml-3"}>
-          {childrenComment &&
-            childrenComment.map((c, i) => (
-              <Comment
-                key={i}
-                comment={c}
-                postId={postId}
-                levelFromRoot={levelFromRoot + 1}
-              />
-            ))}
-        </View>
-      </View>
-    </View>
-  );
-});
-
-export const SkeletonComment = () => {
-  const { theme } = useTheme();
-  return (
-    <Skeleton.Group show={true}>
-      <View className="flex-row gap-3">
-        <Skeleton radius="round" colorMode={theme} width={35} height={35} />
-        <View className="flex-1">
-          <View className="justify-center gap-2 rounded-2xl bg-popover p-4">
-            <Skeleton colorMode={theme} width={150} />
-            <Skeleton colorMode={theme} height={50} width={"100%"} />
-          </View>
-        </View>
-      </View>
-    </Skeleton.Group>
-  );
-};
-=======
->>>>>>> f8a6610 (recursive component and fetching children done)
--- app/(modals)/post/_features/comments.query.ts
+++ app/(modals)/post/_features/comments.query.ts
@@ -28,38 +28,20 @@ const fetcher = async (url: string, token: string) => {
 const getKey = (
   pageIndex: number,
   previousPageData: CommentsData["data"],
-<<<<<<< HEAD
-  postId: string,
-  parentCommentId: number | null
-=======
   postId: string
->>>>>>> f8a6610 (recursive component and fetching children done)
 ) => {
   if (previousPageData && !previousPageData.length) return null;
   return `${
     process.env.EXPO_PUBLIC_API_URL
-<<<<<<< HEAD
-  }/api/post/${postId}/comment?per_page=10&parent_comment_id=${parentCommentId}&page=${
-    pageIndex + 1
-  }`;
-};
-
-export const useComments = (postId: string, parentCommentId: number | null) => {
-=======
   }/api/post/${postId}/comment?per_page=10&page=${pageIndex + 1}`;
 };
 
 export const useComments = (postId: string) => {
->>>>>>> f8a6610 (recursive component and fetching children done)
   const { token } = useAuth();
 
   const res = useFetchInfinite<CommentsData["data"]>(
     (pageIndex, previousPageData) =>
-<<<<<<< HEAD
-      getKey(pageIndex, previousPageData, postId, parentCommentId),
-=======
       getKey(pageIndex, previousPageData, postId),
->>>>>>> f8a6610 (recursive component and fetching children done)
     (url) => fetcher(url, token || "")
   );
   const hasMore = res.data?.[res.data?.length - 1]?.length ?? 0 > 0;
