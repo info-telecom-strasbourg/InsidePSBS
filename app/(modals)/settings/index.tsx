7853be6c96f9651ac0f50eddd7fb722ced9d30e6
Romain Bourdain <romain.bourdain93@gmail.com>
2024-08-30 11:49:51 +0200

    create settings page

--- app/(modals)/settings/index.tsx
+++ app/(modals)/settings/index.tsx
@@ -1,10 +1,12 @@
 import { PageContainer } from "@/components/primitives/container";
 import { Header } from "@/components/primitives/header";
+import { ScrollView } from "react-native";
 
 export default function SettingsPage() {
   return (
     <PageContainer>
       <Header title="Settings" rightIcon="close" />
+      <ScrollView></ScrollView>
     </PageContainer>
   );
 }
