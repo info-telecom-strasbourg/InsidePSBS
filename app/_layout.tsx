d7fcd2508157903de3d4332049e59c306ceada6d
Romain Bourdain <romain.bourdain93@gmail.com>
2024-08-31 11:26:53 +0200

    fix modal display bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { Provider } from "@/components/primitives/providers";
 import { useFonts } from "@/hooks/useFonts";
 import { SplashScreen, Stack } from "expo-router";
-import { View } from "react-native";
 import "../global.css";
 
 SplashScreen.preventAutoHideAsync();
@@ -13,32 +12,21 @@ export default function RootLayout() {
 
   return (
     <Provider>
-      <View className="flex-1 bg-background">
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            animation: "none",
-            contentStyle: { backgroundColor: "transparent" },
-          }}
-        >
-          <Stack.Screen
-            name="(public)"
-            options={{ animation: "slide_from_right" }}
-          />
-          <Stack.Screen
-            name="(dev)/dev"
-            options={{ animation: "fade_from_bottom", presentation: "modal" }}
-          />
-          <Stack.Screen
-            name="(modals)"
-            options={{ animation: "fade_from_bottom", presentation: "modal" }}
-          />
-          <Stack.Screen
-            name="auth"
-            options={{ animation: "fade_from_bottom", presentation: "modal" }}
-          />
-        </Stack>
-      </View>
+      <Stack screenOptions={{ headerShown: false, animation: "none" }}>
+        <Stack.Screen
+          name="(public)/cgu/index"
+          options={{ animation: "fade_from_bottom" }}
+        />
+        <Stack.Screen
+          name="(dev)/dev"
+          options={{ animation: "fade_from_bottom" }}
+        />
+        <Stack.Screen
+          name="(modals)"
+          options={{ animation: "fade_from_bottom" }}
+        />
+        <Stack.Screen name="auth" options={{ animation: "fade_from_bottom" }} />
+      </Stack>
     </Provider>
   );
 }
