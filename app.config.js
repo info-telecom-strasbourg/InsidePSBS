75005dcf57f501ee4a0e7bd8cde5b2956f4b06ff
Siirko <alexander.yanovskyy@gmail.com>
2024-08-31 22:50:27 +0200

    setup submit for android/iOS

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 module.exports = ({ config }) => {
   config.version = process.env.RELEASE_VERSION || '0.0.0';
+  config.ios.buildNumber = process.env.RELEASE_VERSION || '0.0.0';
   return {
     ...config,
   };
