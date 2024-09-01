ca7fa267e86ad6dc8f0aa06585d7a0f15e622295
Siirko <alexander.yanovskyy@gmail.com>
2024-09-01 09:32:30 +0200

    removing letter v from buildNumber of iOS

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = ({ config }) => {
-  config.version = process.env.RELEASE_VERSION || '0.0.0';
-  config.ios.buildNumber = process.env.RELEASE_VERSION || '0.0.0';
+  config.version = process.env.RELEASE_VERSION || 'v0.0.0';
+  config.ios.buildNumber = config.version.substring(1);
   return {
     ...config,
   };
