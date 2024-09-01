508090abfd9766d0e14d4d61ee2b0a2e451e2e21
Siirko <alexander.yanovskyy@gmail.com>
2024-09-01 09:53:30 +0200

    removing letter v from version because iOS :)

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 module.exports = ({ config }) => {
-  config.version = process.env.RELEASE_VERSION || 'v0.0.0';
-  config.ios.buildNumber = config.version.substring(1);
+  const version = process.env.RELEASE_VERSION || 'v0.0.0';
+  config.version = version.substring(1);
+  config.ios.buildNumber = config.version;
   return {
     ...config,
   };
