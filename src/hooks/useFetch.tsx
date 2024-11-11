89a424c4774719a730c3770bda61168aa9dc524a
fab-toc <81991643+fab-toc@users.noreply.github.com>
2024-11-11 21:00:10 +0100

    changed useFetch hook to be able to re-use it when fetching GET requests

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,32 +1,58 @@
-import { useState } from "react";
-import useSWR from "swr";
+import { useAuth } from "@/auth/useAuth";
+import { FetchError, zodFetchWithToken } from "@/utils/fetch";
+import { toastError } from "@/utils/toast";
+import type { QueryOptions } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
 
-export type Fetcher<T> = (url: string) => Promise<T>;
+const fetcher = async (
+  apiEndpoint: string,
+  schema: z.ZodSchema,
+  token: string | null,
+  fetchOptions?: RequestInit
+) => {
+  try {
+    if (!token) throw new Error("Unauthorized");
+    const res = await zodFetchWithToken(apiEndpoint, token, {
+      method: "GET",
+      ...fetchOptions,
+    });
+    const data = await res.json();
 
-export const useFetch = <T,>(url: string, fetcher: Fetcher<T>) => {
-  const { data, error, isLoading, mutate, isValidating } = useSWR(
-    url,
-    fetcher,
-    { revalidateOnFocus: true, revalidateIfStale: false }
-  );
-  const [isRefreshing, setRefreshing] = useState(false);
-
-  const handleRefresh = async () => {
-    setRefreshing(true);
-    try {
-      await mutate();
-    } finally {
-      setRefreshing(false);
+    const parsedData = await schema.parseAsync(data);
+    return parsedData;
+  } catch (error) {
+    if (error instanceof FetchError) {
+      toastError(error.message);
+    } else if (error instanceof z.ZodError) {
+      error.issues.map((e) => ({ path: e.path, message: e.message }));
+      console.error(error);
+    } else {
+      toastError("Une erreur est survenue");
     }
-  };
+    throw error;
+  }
+};
 
-  return {
-    data,
-    error,
-    isLoading,
-    mutate,
-    isValidating,
-    isRefreshing,
-    handleRefresh,
-  };
+export const useFetch = ({
+  apiEndpoint,
+  schema,
+  fetchOptions,
+  queryKey,
+  queryOptions,
+}: {
+  apiEndpoint: string;
+  schema: z.ZodSchema;
+  fetchOptions?: RequestInit;
+  queryKey: string[];
+  queryOptions?: QueryOptions;
+}) => {
+  const { token } = useAuth();
+
+  const res = useQuery({
+    queryKey: queryKey,
+    queryFn: () => fetcher(apiEndpoint, schema, token, fetchOptions),
+    ...queryOptions,
+  });
+  return res;
 };
