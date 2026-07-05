89de737e3436e9074bf1cc96c795294d253a1dd5
fab-toc <81991643+fab-toc@users.noreply.github.com>
2026-07-05 23:41:07 +0200

    chore: delete everything

--- app.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config }) => {
-  const version = process.env.RELEASE_VERSION || 'v0.0.0';
-  config.version = version.substring(1);
-  config.ios.buildNumber = config.version;
-  return {
-    ...config,
-  };
-};
-  
--- src/schemas/auth/forgot.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const ForgotPasswordSchema = z.object({
-  email: z.string().email({ message: "L'email n'est pas valide" }),
-});
-
-export type ForgotPasswordData = z.infer<typeof ForgotPasswordSchema>;
