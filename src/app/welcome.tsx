629e1d55f4fc84e6012ad4cc02524d4c67679768
fab-toc <81991643+fab-toc@users.noreply.github.com>
2026-07-27 17:37:13 +0200

    feat: add base native tabs for navigation & update deps

--- src/app/welcome.tsx
+++ src/app/welcome.tsx
@@ -1,10 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Column, Host } from "@expo/ui";
+import { useRouter } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function WelcomeScreen() {
+  const router = useRouter();
   return (
-    <View style={styles.container}>
-      <Text>Welcome to InsidePSBS !</Text>
-    </View>
+    <Host matchContents>
+      <Column spacing={8}>
+        <Button
+          variant="filled"
+          label="Log In"
+          onPress={() => router.push("/(auth)/log-in")}
+        />
+        <Button variant="outlined" label="Outlined" onPress={() => {}} />
+        <Button
+          variant="text"
+          label="Log In"
+          onPress={() => router.push("/(auth)/log-in")}
+        />
+        {/* <Button onPress={() => {}}>
+          <Row spacing={6} alignment="center">
+            <Icon
+              name={Icon.select({
+                ios: "star.fill",
+                android: require("@expo/material-symbols/star.xml"),
+              })}
+              size={16}
+              color="#FFFFFF"
+            />
+            <Text textStyle={{ color: "#FFFFFF" }}>Favorite</Text>
+          </Row>
+        </Button> */}
+      </Column>
+    </Host>
   );
 }
 
--- src/components/navigation/application-tabs.native.tsx
+++ src/components/navigation/application-tabs.native.tsx
@@ -1,5 +1,78 @@
+import MaterialDesignIcons from "@react-native-vector-icons/material-design-icons";
 import { NativeTabs } from "expo-router/unstable-native-tabs";
 
+const homeIcon = MaterialDesignIcons.getImageSourceSync("home", 24, "black");
+const homeOutlinedIcon = MaterialDesignIcons.getImageSourceSync(
+  "home-outline",
+  24,
+  "black",
+);
+
+const addIcon = MaterialDesignIcons.getImageSourceSync("plus-box", 24, "black");
+const addOutlinedIcon = MaterialDesignIcons.getImageSourceSync(
+  "plus-box-outline",
+  24,
+  "black",
+);
+
+const feedIcon = MaterialDesignIcons.getImageSourceSync(
+  "newspaper-variant",
+  24,
+  "black",
+);
+const feedOutlinedIcon = MaterialDesignIcons.getImageSourceSync(
+  "newspaper-variant-outline",
+  24,
+  "black",
+);
+
+const profileIcon = MaterialDesignIcons.getImageSourceSync(
+  "account",
+  24,
+  "black",
+);
+const profileOutlinedIcon = MaterialDesignIcons.getImageSourceSync(
+  "account-outline",
+  24,
+  "black",
+);
+
 export default function ApplicationTabs() {
-  return <NativeTabs></NativeTabs>;
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label hidden />
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "house", selected: "house.fill" }}
+          src={{ default: homeOutlinedIcon, selected: homeIcon }}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="calendar">
+        <NativeTabs.Trigger.Label hidden />
+        <NativeTabs.Trigger.Icon sf="calendar" md="calendar_today" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="create">
+        <NativeTabs.Trigger.Label hidden />
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "plus.app", selected: "plus.app.fill" }}
+          src={{ default: addOutlinedIcon, selected: addIcon }}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="feed">
+        <NativeTabs.Trigger.Label hidden />
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "newspaper", selected: "newspaper.fill" }}
+          src={{ default: feedOutlinedIcon, selected: feedIcon }}
+        />
+        <NativeTabs.Trigger.Badge>9+</NativeTabs.Trigger.Badge>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "person", selected: "person.fill" }}
+          src={{ default: profileOutlinedIcon, selected: profileIcon }}
+        />
+        <NativeTabs.Trigger.Label hidden />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
